40b84aba12f036116753a49c03f1052eb0ad17cc
Amanda Filizola <amandapaivafilizola@gmail.com>
2019-10-02 13:07:21 -0300

    terminando backend

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,28 @@
+// dependencies
 const express = require('express');
-const SessionController = require('./controllers/SessionController')
-const SpotController = require('./controllers/SpotController')
 const multer = require('multer');
-
+// controller imports
+const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
+// configs and routes
 const routes = express.Router();
 const uploadConfig = require('./config/upload');
 const upload = multer(uploadConfig);
+
+
+// sessions routes
 routes.post('/sessions', SessionController.store);
-routes.post('/spots', upload.single('thumbnail'),SpotController.store);
+
+// spots routes
+routes.get('/spots', SpotController.index);
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+
+// dashboard routes
+routes.get('/dashboard', DashboardController.show);
+
+// booking routes
+routes.post('/spots/:spot_id/bookings', BookingController.store)
 
 module.exports = routes;
