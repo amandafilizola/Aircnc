ca42665411e53b26fc110cf140f117e865901e5f
Amanda Filizola <amandafilizola@polijuniorengenharia.com.br>
2019-10-02 23:01:44 -0300

    adicionando dependencia de cors

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const app = express();
-
+const cors = require('cors');
 mongoose.connect('mongodb+srv://aircnc:omnistack@cluster0-pfp1o.mongodb.net/semana09?retryWrites=true&w=majority',{
     useNewUrlParser:true,
     useUnifiedTopology: true,
@@ -11,6 +11,8 @@ mongoose.connect('mongodb+srv://aircnc:omnistack@cluster0-pfp1o.mongodb.net/sema
 // req.query = acessar  query params (para filtros)
 // req.params = acessar route params (para edição, delete)
 // req.body = acessar corpo da requisição (para criação edição)
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
