42b4a5df9026d88dbcec5095315684e5f6a50f84
Amanda Filizola <amandapaivafilizola@gmail.com>
2019-10-03 15:31:06 -0300

    virtual schema

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const routes = require('./routes');
 const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
+const path = require('path');
+
 mongoose.connect('mongodb+srv://aircnc:omnistack@cluster0-pfp1o.mongodb.net/semana09?retryWrites=true&w=majority',{
     useNewUrlParser:true,
     useUnifiedTopology: true,
@@ -14,6 +16,7 @@ mongoose.connect('mongodb+srv://aircnc:omnistack@cluster0-pfp1o.mongodb.net/sema
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname,'..','uploads')));
 app.use(routes);
 
 app.listen(3333);
