a9be4855f18320b2a921e52e1179951517052df2
Amanda Filizola <amandafilizola@polijuniorengenharia.com.br>
2019-10-06 18:06:48 -0300

    notificações em real time

--- src/server.js
+++ src/server.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
-const app = express();
 const cors = require('cors');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+
 
 mongoose.connect('mongodb+srv://aircnc:omnistack@cluster0-pfp1o.mongodb.net/semana09?retryWrites=true&w=majority',{
     useNewUrlParser:true,
     useUnifiedTopology: true,
 })
 
+const connectedUsers = {};
+io.on('connection', socket => {
+    const { user_id} = socket.handshake.query;
+    connectedUsers[user_id] = socket.id;
+});
+
+// precisa ser antes das rotas do applicativo!
+app.use((req,res, next)=>{
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
 // req.query = acessar  query params (para filtros)
 // req.params = acessar route params (para edição, delete)
 // req.body = acessar corpo da requisição (para criação edição)
@@ -18,5 +38,4 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname,'..','uploads')));
 app.use(routes);
-
-app.listen(3333);
+server.listen(3333);
